9acaf15bf6617db5d5afd59b2df43cc333657142
Nishimiya <dnk.hidayat@gmail.com>
2024-09-24 17:10:00 +0700

    Simplified function naming

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Function to check if yay is installed
-check_yay_installed() {
+# Check if yay is installed
+yay_check() {
     if command -v yay &> /dev/null; then
         echo -e "\e[1;32m:: yay is already installed. Skipping installation...\e[0m"
         return 0
@@ -10,15 +10,15 @@ check_yay_installed() {
     fi
 }
 
-# Function to configure sudo to cache the password
-configure_sudo() {
+# Configure sudo to cache the password
+sudo_cache() {
     echo -e "\e[1;32m:: Configuring sudo to cache the password... [1/11]\e[0m"
     echo "Defaults timestamp_timeout=120" | sudo tee -a /etc/sudoers.d/password_timeout
 }
 
-# Function to install yay AUR helper
-install_yay() {
-    if check_yay_installed; then
+# Install yay AUR helper
+yay_install() {
+    if yay_check; then
         return
     fi
 
@@ -27,12 +27,12 @@ install_yay() {
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si --noconfirm
-    cd ..
+    cd .. 
     rm -rf yay
 }
 
-# Function to install packages from the package list
-install_packages() {
+# Install packages from the package list
+pkg_install() {
     echo -e "\e[1;32m:: Installing packages... [3/11]\e[0m"
     if [ ! -f packages ]; then
         echo -e "\e[1;31mError: packages not found. Attempting to download from remote...\e[0m"
@@ -47,16 +47,16 @@ install_packages() {
     yay -S --needed --noconfirm $(cat packages)
 }
 
-# Function to clone dotfiles repository
-clone_dotfiles() {
+# Clone dotfiles repository
+dotfiles_clone() {
     echo -e "\e[1;32m:: Cloning dotfiles repository... [4/11]\e[0m"
     REPO_NAME=$(basename -s .git https://github.com/xangelkawaiix/hyprland-dotfiles)
     git clone https://github.com/xangelkawaiix/hyprland-dotfiles.git "$REPO_NAME"
     cd "$REPO_NAME"
 }
 
-# Function to copy configuration files and folders
-copy_configs() {
+# Copy configuration files and folders
+configs_copy() {
     echo -e "\e[1;32m:: Copying configuration files and folders... [5/11]\e[0m"
     cp -r .config/* ~/.config/
     echo -e "\e[1;34mCopying .config/* -> ~/.config/\e[0m"
@@ -70,14 +70,14 @@ copy_configs() {
     ln -sf ~/.config/zsh/.zprofile ~/.zprofile
 }
 
-# Function to create user folders using xdg-user-dirs-update
-create_user_folders() {
+# Create user folders using xdg-user-dirs-update
+user_dirs() {
     echo -e "\e[1;32m:: Creating user directories... [6/11]\e[0m"
     xdg-user-dirs-update
 }
 
-# Function to configure Arch Linux mirror list
-configure_mirrors() {
+# Configure Arch Linux mirror list
+mirrors_config() {
     while true; do
         read -p "Do you want to configure the Arch Linux mirror list? (y/n): " response
         case $response in
@@ -95,8 +95,8 @@ configure_mirrors() {
     done
 }
 
-# Function to set timedatectl for Windows dual boot
-set_timedatectl() {
+# Set timedatectl for Windows dual boot
+timedatectl_set() {
     while true; do
         read -p "Do you want to set timedatectl for Windows dual boot? (y/n): " response
         case $response in
@@ -113,8 +113,8 @@ set_timedatectl() {
     done
 }
 
-# Function to configure gpg-agent
-configure_gpg_agent() {
+# Configure gpg-agent
+gpg_config() {
     while true; do
         read -p "Do you want to configure gpg-agent? (y/n): " response
         case $response in
@@ -135,33 +135,33 @@ configure_gpg_agent() {
     done
 }
 
-# Function to change shell to zsh and install starship
-change_shell_and_install_starship() {
+# Change shell to zsh and install starship
+zsh_starship() {
     echo -e "\e[1;32m:: Changing shell to zsh... [10/11]\e[0m"
     chsh -s /bin/zsh
     echo -e "\e[1;32m:: Installing starship...\e[0m"
     sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 }
 
-# Function to run os-prober (mandatory)
-run_os_prober() {
+# Run os-prober
+os_prober_run() {
     echo -e "\e[1;32m:: Running os-prober... [11/11]\e[0m"
     sudo os-prober
 }
 
 # Main function
 main() {
-    configure_sudo
-    install_yay
-    install_packages
-    clone_dotfiles
-    copy_configs
-    create_user_folders
-    configure_mirrors
-    set_timedatectl
-    configure_gpg_agent
-    change_shell_and_install_starship
-    run_os_prober
+    sudo_cache
+    yay_install
+    pkg_install
+    dotfiles_clone
+    configs_copy
+    user_dirs
+    mirrors_config
+    timedatectl_set
+    gpg_config
+    zsh_starship
+    os_prober_run
     echo -e "\e[1;32mBootstrap complete!\e[0m"
 }
 
